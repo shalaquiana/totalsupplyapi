5f888e80d20f54576bcc89dd99d7f95e090a7e47
Sha <knott@Knotts-MacBook-Pro.local>
2021-06-05 14:42:39 -0400

    add basic circulating supply

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var Web3 = require('web3');
+
+const express = require('express');
 var http = require('http');
 var id = "PASTE_ID_IN_THESE_QUOTATIONS";
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${id}`))
-
+var BN = web3.utils.BN;
 const TOKEN_ADDRESS = "0x29641e1096d4240b09a933839feac57b200652a4";
+const app = express();
 
-async function getData() {
+
+
+async function getTotalSupply() {
     return new Promise(async (resolve, reject) => {
         const bp = new web3.eth.Contract(
             [
@@ -426,14 +431,443 @@ async function getData() {
           )
       })
 }
-  
 
-var server = http.createServer(async function  (req, res) {
-    var ts = await getData();
-    res.write(ts);
-    res.end();
+async function getSafeBalance() {
+  return new Promise(async (resolve, reject) => {
+      const bp = new web3.eth.Contract(
+          [
+              {
+                "inputs": [
+                  {
+                    "internalType": "string",
+                    "name": "name_",
+                    "type": "string"
+                  },
+                  {
+                    "internalType": "string",
+                    "name": "symbol_",
+                    "type": "string"
+                  }
+                ],
+                "stateMutability": "nonpayable",
+                "type": "constructor"
+              },
+              {
+                "anonymous": false,
+                "inputs": [
+                  {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                  },
+                  {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                  },
+                  {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "value",
+                    "type": "uint256"
+                  }
+                ],
+                "name": "Approval",
+                "type": "event"
+              },
+              {
+                "anonymous": false,
+                "inputs": [
+                  {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "from",
+                    "type": "address"
+                  },
+                  {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "to",
+                    "type": "address"
+                  },
+                  {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "value",
+                    "type": "uint256"
+                  }
+                ],
+                "name": "Transfer",
+                "type": "event"
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                  },
+                  {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                  }
+                ],
+                "name": "allowance",
+                "outputs": [
+                  {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function",
+                "constant": true
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address",
+                    "name": "account",
+                    "type": "address"
+                  }
+                ],
+                "name": "balanceOf",
+                "outputs": [
+                  {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function",
+                "constant": true
+              },
+              {
+                "inputs": [],
+                "name": "decimals",
+                "outputs": [
+                  {
+                    "internalType": "uint8",
+                    "name": "",
+                    "type": "uint8"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function",
+                "constant": true
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                  },
+                  {
+                    "internalType": "uint256",
+                    "name": "subtractedValue",
+                    "type": "uint256"
+                  }
+                ],
+                "name": "decreaseAllowance",
+                "outputs": [
+                  {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                  }
+                ],
+                "stateMutability": "nonpayable",
+                "type": "function"
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                  },
+                  {
+                    "internalType": "uint256",
+                    "name": "addedValue",
+                    "type": "uint256"
+                  }
+                ],
+                "name": "increaseAllowance",
+                "outputs": [
+                  {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                  }
+                ],
+                "stateMutability": "nonpayable",
+                "type": "function"
+              },
+              {
+                "inputs": [],
+                "name": "name",
+                "outputs": [
+                  {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function",
+                "constant": true
+              },
+              {
+                "inputs": [],
+                "name": "owner",
+                "outputs": [
+                  {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function",
+                "constant": true
+              },
+              {
+                "inputs": [],
+                "name": "symbol",
+                "outputs": [
+                  {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function",
+                "constant": true
+              },
+              {
+                "inputs": [],
+                "name": "totalSupply",
+                "outputs": [
+                  {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function",
+                "constant": true
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address",
+                    "name": "recipient",
+                    "type": "address"
+                  },
+                  {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                  }
+                ],
+                "name": "transfer",
+                "outputs": [
+                  {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                  }
+                ],
+                "stateMutability": "nonpayable",
+                "type": "function"
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address",
+                    "name": "sender",
+                    "type": "address"
+                  },
+                  {
+                    "internalType": "address",
+                    "name": "recipient",
+                    "type": "address"
+                  },
+                  {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                  }
+                ],
+                "name": "transferFrom",
+                "outputs": [
+                  {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                  }
+                ],
+                "stateMutability": "nonpayable",
+                "type": "function"
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                  }
+                ],
+                "name": "whitelist",
+                "outputs": [
+                  {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function",
+                "constant": true
+              },
+              {
+                "inputs": [],
+                "name": "whitelistEnabled",
+                "outputs": [
+                  {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function",
+                "constant": true
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                  }
+                ],
+                "name": "burn",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address payable",
+                    "name": "newOwner_",
+                    "type": "address"
+                  }
+                ],
+                "name": "transferOwner",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address payable",
+                    "name": "user",
+                    "type": "address"
+                  }
+                ],
+                "name": "addToWhitelist",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address payable",
+                    "name": "user",
+                    "type": "address"
+                  }
+                ],
+                "name": "removeFromWhitelist",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+              },
+              {
+                "inputs": [],
+                "name": "disableWhitelist",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+              },
+              {
+                "inputs": [
+                  {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                  },
+                  {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                  }
+                ],
+                "name": "approve",
+                "outputs": [
+                  {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                  }
+                ],
+                "stateMutability": "nonpayable",
+                "type": "function"
+              }
+            ],
+          TOKEN_ADDRESS
+        )
+      await bp.methods
+        .balanceOf("0x784aC242f2e5F468EDd54c9a2b46d4f930B52A08")
+        .call(
+          { from: "0x0000000000000000000000000000000000000000" },
+          (err, data) => {
+            if (err) {
+                console.log("an error occured")
+                console.log(err)
+                reject(err);
+            }
+            resolve(data);
+          }
+        )
+    })
+}
+  
+app.get('/totalSupply', async (req, res) => {
+  var ts = await getTotalSupply();
+  res.send(ts);
 });
 
-server.listen(8080);
+app.get('/circulatingSupply', async (req, res) => {
 
-console.log(`Total Supply web server for token at ${TOKEN_ADDRESS} at port 8080 is running..`)
+  var ts = await getTotalSupply();
+  var safe = await getSafeBalance();
+  var circulating = new BN(ts).sub(new BN(safe)).toString();
+  res.send(circulating);
+})
+
+app.listen(8080), () => {
+
+  console.log(`Total Supply web server for token at ${TOKEN_ADDRESS} at port 8080 is running..`)
+}
